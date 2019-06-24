6959c8f713e14961c0e8ebe292ff83b90224431e
Billy Chen <billy.chen1206@gmail.com>
2019-06-24 16:33:04 -0400

    fixed results display

--- server.rb
+++ server.rb
@@ -1,16 +1,16 @@
 require "sinatra"
 require "googlebooks"
 
-# get "/" do
-#   erb :home
-# end
-#
-# get "/about" do
-#   erb :about
-# end
-
 get "/" do
+  erb :home
+end
+
+get "/about" do
+  erb :about
+end
+
+get "/results" do
   @books = GoogleBooks.search("#{params[:query]}")
   @first = @books.first
-  erb :home
+  erb :results
 end
